9d1ad5eca9aee25da1e3349c7cf8bcf66e04a516
taotao820 <xht820@gmail.com>
2021-05-07 21:11:04 +0800

    commit

---
+++ src/constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x260d2E9ee9867Eed08c594F3619cBdC6A576EfdA'
+
+export const INIT_CODE_HASH = '0x67afece463b64891c13546ab1249f241b6654938a3a09c4ecae813bdc0128ff2'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
